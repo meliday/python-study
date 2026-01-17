4a7ca746a7ec15a933a956b0949bf76011ea6b81
voidlaniakea <yjmksh777@naver.com>
2026-01-17 19:51:58 +0900

    add about list and revise readme

---
+++ 01_data_types/list_methods.py
@@ -0,0 +1,18 @@
+#Study About List
+str = "Hello World! My name is Songhun"
+list = str.split()
+num_list = [12,445,213124,23,45512]
+num_list.sort()
+print(num_list)
+num_list.reverse()
+print(num_list)
+print(list.index("My"))
+num_list.insert(0, 4)
+print(num_list)
+num_list.remove(4)
+print(num_list)
+print(num_list.pop())
+print(num_list)
+num_list.extend(list)
+print(num_list)
+print(len(num_list))
